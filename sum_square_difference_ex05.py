334a4f9da86ec043dc0a4e4a818381e70f782813
Irwing <irwing.devbr@gmail.com>
2025-03-07 19:04:30 -0300

    feat: add the fifth exercise, the sum square difference exercise

---
+++ sum_square_difference_ex05.py
@@ -0,0 +1,35 @@
+""""
+The sum of the squares of the first ten natural numbers is,
+
+                        1² + 2² + ... + 10² = 385
+
+The square of the sum of the first ten natural numbers is,
+
+                        (1 + 2 + ... + 10)² = 55² = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+# Code
+
+def sum_of_the_squares():
+    squares = []
+    sum_sqaures = 0
+    for i in range(1, 101):
+        squares.append(i**2)
+    for n in squares:
+        sum_sqaures += n
+    return sum_sqaures
+
+def squares_of_the_sum():
+    numbers = []
+    sum_numbers = 0
+    for i in range(1, 101):
+        numbers.append(i)
+    for n in numbers:
+        sum_numbers += n
+    return sum_numbers**2
+
+print(f"The square of the sum is: {squares_of_the_sum()} and the sum of the squares is: {sum_of_the_squares()} \nThe subtraction of them is: {squares_of_the_sum() - sum_of_the_squares()}")
