0607fb3e6946abb1db274c04746335e34e4a208a
Irwing <irwing.devbr@gmail.com>
2025-03-15 16:43:50 -0300

    feat: add largest product in a series exercise, 08
    This commit adds the 8th exercise and renamed the
    9th exercise: special_pythagorean_triplet.py for
    special_pythagorean_triplet_ex09.py

--- largest_palindrome_product_ex04.py
+++ largest_palindrome_product_ex04.py
@@ -4,6 +4,8 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
+#Code
+
 class Palindromer:
     def __init__(self, n, mult, res):
         self.n = n
--- largest_prime_factor_ex03.py
+++ largest_prime_factor_ex03.py
@@ -4,6 +4,8 @@ The prime factors of 13295 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 """
 
+# Code
+
 def prime_factors(n):
     factors = []
     divider = 2
